4c1ec534dff6f80a67328c0bc36afa0763fa1f2c
vtonu <victor.tonu@yahoo.com>
2023-12-24 13:54:35 -0800

    Initial commit, added background, fixed font, added buttons and blur effect using backdrop-filter.

---
+++ src/GlassUI.jsx
@@ -0,0 +1,17 @@
+export default function GlassUI() {
+  return (
+    <div>
+      <div className="upper-layer">
+        <div className="atmHeader">
+          <h1>Bank Account</h1>
+          <h6>Active</h6>
+          <h5>Manage your account balance.</h5>
+        </div>
+        <div className="bodyHeader">
+          <button className="button">Deposit</button>
+          <button className="button">Withdraw</button>
+        </div>
+      </div>
+    </div>
+  );
+}
