accd38b56722381017fa0f0a9fce5bc137ea3ba0
vtonu <victor.tonu@yahoo.com>
2023-12-24 14:38:59 -0800

    Made the clear button functional.

--- src/GlassUI.jsx
+++ src/GlassUI.jsx
@@ -1,13 +1,29 @@
-export default function GlassUI() {
+import { useState } from 'react';
+
+function GlassUI() {
+  const [inputValue, setInputValue] = useState('');
+
+  const clearInput = () => {
+    setInputValue('');
+  };
+
   return (
     <div>
       <div className="upper-layer">
         <div className="atmHeader">
           <h1>Bank Account</h1>
-          <h6>Active</h6>
           <h5>Manage your account balance.</h5>
+          <h6>Active</h6>
         </div>
-        <div className="bodyHeader">
+        <div className="accountBalance">
+          <h5>Current Balance:</h5>
+          <h1>$ 758.00</h1>
+          <input type="number" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        </div>
+        <div className="buttonsHeader">
+          <button className="button" onClick={clearInput}>
+            Clear
+          </button>
           <button className="button">Deposit</button>
           <button className="button">Withdraw</button>
         </div>
@@ -15,3 +31,5 @@ export default function GlassUI() {
     </div>
   );
 }
+
+export default GlassUI;
